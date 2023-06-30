7dc8a968207089b9a51026a605a34ea6e7a37923
shailesh <shailesh.d@simformsolutions.com>
2023-06-30 11:23:30 +0530

    Second Level Cache is implemented

--- src/main/java/com/hibernate/cache/entity/Course.java
+++ src/main/java/com/hibernate/cache/entity/Course.java
@@ -6,13 +6,15 @@ import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.Cache;
+import org.hibernate.annotations.CacheConcurrencyStrategy;
 
 
 @Entity
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-
+@Cache(usage= CacheConcurrencyStrategy.READ_ONLY, region="course")
 public class Course {
     @Id
     private Integer courseId;
